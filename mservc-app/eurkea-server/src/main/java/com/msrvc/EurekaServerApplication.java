7d774dd0f3591789ea1af9cf7d5e1e03793d5733
Ashwin Balaji Dhandapani <dhandapani.as@northeastern.edu>
2024-01-10 01:46:48 -0500

    eureka server added for communication between services

--- mservc-app/eurkea-server/src/main/java/com/msrvc/EurekaServerApplication.java
+++ mservc-app/eurkea-server/src/main/java/com/msrvc/EurekaServerApplication.java
@@ -2,12 +2,12 @@ package com.msrvc;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
-@EnableEurekaClient
-public class CustomerApplication {
+@EnableEurekaServer
+public class EurekaServerApplication {
     public static void main(String[] args) {
-        SpringApplication.run(CustomerApplication.class, args);
+        SpringApplication.run(EurekaServerApplication.class, args);
     }
 }
