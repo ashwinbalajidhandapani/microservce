48fec2deb4d173d1364d16e249f1174a62ca73a9
Ashwin Balaji Dhandapani <dhandapani.as@northeastern.edu>
2024-01-09 23:53:31 -0500

    email check service

--- mservc-app/customer/src/main/java/com/msrvc/CustomerService.java
+++ mservc-app/customer/src/main/java/com/msrvc/CustomerService.java
@@ -1,16 +1,33 @@
 package com.msrvc;
 
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 @Service
-public record CustomerService(CustomerRepository customerRepository) {
+@AllArgsConstructor
+
+public class CustomerService {
+
+    private final CustomerRepository customerRepository;
+    private final RestTemplate restTemplate;
     public void registerCustomer(CustomerRegistrationRequest request) {
         Customer customer = Customer.builder()
                 .firstName(request.firstName())
                 .lastName(request.lastName())
                 .email(request.email())
                 .build();
+        customerRepository.saveAndFlush(customer);
+        FraudCheckResponse fraudCheckResponse = restTemplate.getForObject(
+                "http://localhost:8081/api/v1/fraud-check/{customerId}",
+                FraudCheckResponse.class,
+                customer.getId()
+        );
 
-        customerRepository.save(customer);
+        if(fraudCheckResponse.isFraudster()){
+            throw new IllegalStateException("Fraud");
+        }
+
     }
 }
---
+++ mservc-app/customer/src/main/java/com/msrvc/FraudCheckResponse.java
@@ -0,0 +1,4 @@
+package com.msrvc;
+
+public record FraudCheckResponse(Boolean isFraudster) {
+}
